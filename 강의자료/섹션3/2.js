cd473703c40408e324e0a72197922f28503abb8f
dudgp128 <20181048@sungshin.ac.kr>
2023-10-09 15:35:36 +0900

    섹션3 2번 문제 다시 풀기

--- 강의자료/섹션3/2.js
+++ 강의자료/섹션3/2.js
@@ -1,8 +1,14 @@
 function solution(s) {
-  let lower_s = s.toLowerCase();
-  let filtered = lower_s.replace(/[^a-z ]/g, "");
-  let reversed = filtered.split("").reverse().join("");
-  return filtered === reversed ? "YES" : "NO";
+  let big_text = s.toUpperCase();
+  let array = big_text.split(" ");
+  const regexp = /[A-Z]/g;
+
+  let result = [];
+  for (let e of array) {
+    let text = e.match(regexp);
+    result.push(text);
+  }
+  return result === result.reverse();
 }
 
 let str = "found7, time: study; Yduts; emit, 7Dnuof";
