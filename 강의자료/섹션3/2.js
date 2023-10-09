152c5848fb3382d6ccf25d2a0d4cf8395e8bf9ad
dudgp128 <20181048@sungshin.ac.kr>
2023-10-09 15:42:29 +0900

    섹션 3 2번 문제 리팩토링

--- 강의자료/섹션3/2.js
+++ 강의자료/섹션3/2.js
@@ -1,14 +1,8 @@
 function solution(s) {
   let big_text = s.toUpperCase();
-  let array = big_text.split(" ");
-  const regexp = /[A-Z]/g;
-
-  let result = [];
-  for (let e of array) {
-    let text = e.match(regexp);
-    result.push(text);
-  }
-  return result === result.reverse();
+  let filtered = big_text.replace(/[^A-Z]/g, "");
+  let reveresed = filtered.split("").reverse().join("");
+  return filtered === reveresed ? "YES" : "NO";
 }
 
 let str = "found7, time: study; Yduts; emit, 7Dnuof";
