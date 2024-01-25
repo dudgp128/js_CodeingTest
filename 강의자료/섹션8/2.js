1838c805b5f0987b363313b79edd8f58ae10f866
dudgp128 <nbv19@naver.com>
2024-01-25 12:42:22 +0900

    십진수를 이진수로 변환하기

--- 강의자료/섹션8/2.js
+++ 강의자료/섹션8/2.js
@@ -1,19 +1,5 @@
 function solution(n) {
-  let result = [];
-
-  a(n);
-
-  function a(n) {
-    if (n === 1) {
-      return result.unshift(1);
-    } else {
-      result.unshift(n % 2);
-      n = Math.floor(n / 2);
-      return a(n);
-    }
-  }
-
-  return result.join("");
+  return n.toString(2);
 }
 
 console.log(solution(11));
