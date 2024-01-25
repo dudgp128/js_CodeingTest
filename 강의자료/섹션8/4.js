b39e325d0941a20004d5fc897d539f891d76c05e
dudgp128 <nbv19@naver.com>
2024-01-25 12:42:57 +0900

    [DFS] 부분 집합 출력하기

--- 강의자료/섹션8/4.js
+++ 강의자료/섹션8/4.js
@@ -1,20 +1,26 @@
 function solution(n) {
-  let stack = Array.from({ length: n + 1 }, () => 0);
-  //DFS(1)
-  function DFS(v) {
-    if (n + 1 === v) {
-      let tmp = "";
-      for (let i = 1; i < n + 1; i++) {
-        if (stack[i] === 1) {
-          tmp += i + " ";
-        }
-        console.log(tmp);
+  let arr = Array.from({ length: n }, () => 0);
+
+  function DFS(L) {
+    if (L === n) {
+      let result = arr
+        .map((el, index) => {
+          if (el === 1) {
+            return index + 1;
+          } else {
+            return;
+          }
+        })
+        .filter((el) => el !== undefined);
+      if (result.length) {
+        console.log(result);
       }
+      return;
     } else {
-      stack[v] = 1;
-      DFS(v + 1);
-      stack[v] = 0;
-      DFS(v + 1);
+      arr[L] = 1;
+      DFS(L + 1);
+      arr[L] = 0;
+      DFS(L + 1);
     }
   }
 }
